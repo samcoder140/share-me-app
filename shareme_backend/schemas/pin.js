4f3dd6d767433ab755bebbdc5b73969fab77fc0c
samcoder140 <smiller@theia.io>
2022-01-12 16:17:55 -0800

    install sanity CLI, start setting up database schemas

---
+++ shareme_backend/schemas/pin.js
@@ -0,0 +1,22 @@
+export default {
+  name: "pin",
+  title: "Pin",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "about",
+      title: "About",
+      type: "string",
+    },
+    {
+      name: "destination",
+      title: "Destination",
+      type: "string",
+    },
+  ],
+};
