f9956599171b7dd1e7e6b04c96bd46d4afeafb1e
samcoder140 <smiller@theia.io>
2022-01-16 13:07:41 -0800

    work on feed page, search queries, feed queries, and spinner (SPINNER NOT WORKING - PROBABLY NEEDS ANOTHER IMPLEMENTATION)

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { RiH1 } from "react-icons/ri";
+import { useParams } from "react-router-dom";
+
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 };
 
 export default Feed;
